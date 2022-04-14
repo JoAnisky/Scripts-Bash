5f6dbed9f017a69f975263ce6fc686fd733dc8d8
JoAnisky <anisky5@gmail.com>
2022-04-14 16:38:41 +0200

    rajout de commentaires d'explications dans le script

--- html_generator.sh
+++ html_generator.sh
@@ -1,8 +1,16 @@
 #!bin/bash
+""
+    # On commence par récuperer le nom du fichier .html que l'on stocke dans une variable $htmlfilename 
 read -r -p "Entrez un nom pour votre fichier .html (sans ecrire .html) : " htmlfilename
+    # Idem pour le nom du fichier .css => variable $cssfilename
 read -r -p "Entrez un nom pour votre fichier .css (sans ecrire .css) : " cssfilename
+    # On récupère le titre de la page avec la variable $pagetitle qui sera inséré entre les balises <title></title>
 read -r -p "Entrez le titre de votre page html : " pagetitle
+    # Et enfin on demande le chemin ou l'utilisateur souhaite enregistrer son fichier. Par défaut on se situe la ou est placé le script 
+    # (POINTS D'AMELIORATION A FAIRE SUR CE SUJET !!!)
 read -r -p "Où voulez-vous enregistrer le fichier ? " workingdirectory
+# La suite (L14) crée un fichier avec le chemin et le nom de fichier choisi (par défaut .html)
+# et génère la structure de base du site (L15 à L28) en remplissant les balises title et le href du fichier css
 cat << EOF > $workingdirectory/$htmlfilename.html
 <!DOCTYPE html>
 <html lang="fr">
@@ -19,6 +27,7 @@ cat << EOF > $workingdirectory/$htmlfilename.html
 </body>
 </html>
 EOF
+# On génère ici la feuille de style .css, en récuperant la variable $cssfilename pour le nom du fichier
 cat << EOF > $workingdirectory/$cssfilename.css
 body{
     margin:0;
