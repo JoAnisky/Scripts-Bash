409bc0798fe1f94f8da9920bada151f8297bc9ac
Elwindria <p.lopez2000@laposte.net>
2022-04-15 10:56:26 +0200

    add while

--- html_generator.sh
+++ html_generator.sh
@@ -10,23 +10,30 @@ read -r -p "Entrez le titre de votre page html : " pagetitle
     # (POINTS D'AMELIORATION A FAIRE SUR CE SUJET !!!)
 read -r -p "Nommez le dossier où vous voulez enregistrer le projet ? " workingdirectory
 
+
 if [[ ! -d "$workingdirectory" ]]
 then
         if [[ ! -L $workingdirectory ]]
         then
-                read -r -p "Le dossier n'existe pas. Voulez-vous le créer ?" reponse
-                mkdir $dirname
-                echo "Directory created"  
+                read -r -p "Le dossier n'existe pas. Voulez-vous le créer [y/n]?" reponse
+                case "$reponse" in
+                    oui | Oui | OUI | o | O | yes | Yes | YES | y | Y ) mkdir $workingdirectory echo "[x]Dossier créé";;
+                    non | Non | NON | n | N | no | NO | No | n | N ) echo "Retour au choix de dossier" i=0;;
+                    *) echo "Error syntax !" i=0;;
+                esac 
         fi
 else
-    echo "Directory exists" 
+    echo "Le dossier existe déjà" 
 fi
 
+read -r -p "Les fichiers seront enregistrés dans $PWD/$workingdirectory. Voulez-vous continuer ? [Y/N] : " continuer
 
-read -r -p "Le dossier sera enregistrer dans $PWD/$workingdirectory [Y/N] : " reponse
+case "$continuer" in
+        oui | Oui | OUI | o | O | yes | Yes | YES | y | Y ) echo "[x]Fichier HTML crée" echo "[x]Fichier Css crée" echo "[x]Fichier Js crée";;
+        non | Non | NON | n | N | no | NO | No | n | N ) echo "Retour au choix de dossier" i=0;;
+        *) echo "Error syntax !" i=0;;
+esac 
 
-case "$reponse" in
-    oui | Oui | OUI | o | O | yes | Yes | YES | y | Y ) 
 # La suite (L14) crée un fichier avec le chemin et le nom de fichier choisi (par défaut .html)
 # et génère la structure de base du site (L15 à L28) en remplissant les balises title et le href du fichier css
 cat << EOF > $workingdirectory/$htmlfilename.html
