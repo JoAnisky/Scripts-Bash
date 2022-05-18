1676fdc1a4f9ac6b7db036f245dd58cb3305e05b
Elwindria <p.lopez2000@laposte.net>
2022-05-18 11:58:55 +0200

    Ajout de if pour permettre la création des fichier dans le dossier actuel, et non plus devoir toujours créer un nouveau dossier.

--- html_generator.sh
+++ html_generator.sh
@@ -12,14 +12,32 @@ read -r -p "Entrez le titre de votre page html : " pagetitle
 
 
 #ajout d'un while pour demander un dossier de projet. Temps que l'utilisateur n'est pas en accord, on lui redemande.
+#création de la var "y" pour définir si l'utilisateur veut utilser le dossier où il est, ou non. 
 p=1
+y=1
 while [ $p = 1 ]
 do
 
 i=1
 while [ $i = 1 ]
 do
-read -r -p "Nommez le dossier où vous voulez enregistrer le projet. $PWD/ " workingdirectory
+
+
+read -r -p "Voulez vous utiliser le dossier où vous êtes pour enregistrer le projet ? $PWD : " reponse
+    case "$reponse" in
+        oui | Oui | OUI | o | O | yes | Yes | YES | y | Y )
+        #Je voulais donner à ma variable workingdirectory la valeur ==> $(basename $pwd) à la base pour
+        #ne pas modifier la suite du code. Mais dans ces condition, le programme comprend pas car il cherche
+        #un dossier qui ce nomme comme le dossier actuel, mais dans le dossier actuel !
+        # Ex: admin/projet1 ==> il cherchera le chemin admin/projet1/projet1 ... Et il prendra pas le projet1 existant
+        #C'est très triste alors du coup, j'ai fais un bon gros if à la fin avec "y", et je change les chemin manuellement
+        y=0
+        break ;;
+        non | Non | NON | n | N | no | NO | No | n | N )
+        read -r -p "Nommez le dossier où vous voulez enregistrer le projet. " workingdirectory ;;
+        *) echo "Error syntax !" ;;
+    esac 
+
 if [[ ! -d "$workingdirectory" ]]
 then
         if [[ ! -L $workingdirectory ]]
@@ -44,104 +62,209 @@ else
 fi
 done
 
-#ajout d'une autre boucle while, pour la confirmation ultime
+#ajout d'une autre boucle while, pour la confirmation ultime avec un if qui vérifie la var "y" pour donner le bon chemin
+#en fonction de si il choisi un nouveau dossier ou le dossier actuel. 
 
-read -r -p "On utilisera le chemin suivant pour la création des fichiers : $PWD/$workingdirectory. Voulez-vous continuer ? [Y/N] : " continuer
+if [ $y = 1 ]
+then
+    read -r -p "On utilisera le chemin suivant pour la création des fichiers : $PWD/$workingdirectory. Voulez-vous continuer ? [Y/N] : " continuer
 
-case "$continuer" in
-        oui | Oui | OUI | o | O | yes | Yes | YES | y | Y )
-        p=0
-        mkdir $workingdirectory/images ;;
-        non | Non | NON | n | N | no | NO | No | n | N ) 
-        echo "Retour au début.";;
-        *) 
-        echo "Error syntax !" ;;
-esac 
+    case "$continuer" in
+            oui | Oui | OUI | o | O | yes | Yes | YES | y | Y )
+            p=0
+            mkdir $workingdirectory/images ;;
+            non | Non | NON | n | N | no | NO | No | n | N ) 
+            echo "Retour au début.";;
+            *) 
+            echo "Error syntax !" ;;
+    esac 
+else 
+    read -r -p "On utilisera le chemin suivant pour la création des fichiers : $PWD. Voulez-vous continuer ? [Y/N] : " continuer
 
+    case "$continuer" in
+            oui | Oui | OUI | o | O | yes | Yes | YES | y | Y )
+            p=0
+            mkdir images ;;
+            non | Non | NON | n | N | no | NO | No | n | N ) 
+            echo "Retour au début.";;
+            *) 
+            echo "Error syntax !" ;;
+    esac 
+fi
 done
 
-while true
-do
+if [ $y = 1 ]
+then
 
-read -r -p "Voulez vous créer un sous-dossier JavaScript ? : " js
+    while true
+    do
+    read -r -p "Voulez vous créer un sous-dossier JavaScript ? : " js
 
-case "$js" in
-        oui | Oui | OUI | o | O | yes | Yes | YES | y | Y )
-        mkdir $workingdirectory/js
-        echo "[x] Dossier JS créé"
-        read -r -p "Entrez un nom pour votre fichier .js (sans écrire le .js) : " jsfilename
-        break ;;
-        non | Non | NON | n | N | no | NO | No | n | N )
-        break ;;
-        *) 
-        echo "Error syntax !" ;;
-esac 
-done
+    case "$js" in
+            oui | Oui | OUI | o | O | yes | Yes | YES | y | Y )
+            mkdir $workingdirectory/js
+            echo "[x] Dossier JS créé"
+            read -r -p "Entrez un nom pour votre fichier .js (sans écrire le .js) : " jsfilename
+            break ;;
+            non | Non | NON | n | N | no | NO | No | n | N )
+            break ;;
+            *) 
+            echo "Error syntax !" ;;
+    esac 
+    done
 
-while true
-do
+    while true
+    do
 
-read -r -p "Voulez vous créer un sous-dossier php ? : " php
+    read -r -p "Voulez vous créer un sous-dossier php ? : " php
 
-case "$php" in
-        oui | Oui | OUI | o | O | yes | Yes | YES | y | Y )
-        mkdir $workingdirectory/php
-        echo "[x] Dossier php créé"
-        break;;
-        non | Non | NON | n | N | no | NO | No | n | N )
-        break ;;
-        *) 
-        echo "Error syntax !" ;;
-esac 
-done
+    case "$php" in
+            oui | Oui | OUI | o | O | yes | Yes | YES | y | Y )
+            mkdir $workingdirectory/php
+            echo "[x] Dossier php créé"
+            break;;
+            non | Non | NON | n | N | no | NO | No | n | N )
+            break ;;
+            *) 
+            echo "Error syntax !" ;;
+    esac 
+    done
 
-while true 
-do
+    while true 
+    do
 
-read -r -p "Voulez vous créer un sous-dossier FONTS ? [Y/N] : " fonts 
+    read -r -p "Voulez vous créer un sous-dossier FONTS ? [Y/N] : " fonts 
 
-case "$fonts" in
-        oui | Oui | OUI | o | O | yes | Yes | YES | y | Y )
-        mkdir $workingdirectory/fonts
-        echo "[x] Dossier fonts créé"
-        break;;
-        non | Non | NON | n | N | no | NO | No | n | N ) 
-        break;;
-        *) 
-        echo "Error syntax !" ;;
-esac 
-done
+    case "$fonts" in
+            oui | Oui | OUI | o | O | yes | Yes | YES | y | Y )
+            mkdir $workingdirectory/fonts
+            echo "[x] Dossier fonts créé"
+            break;;
+            non | Non | NON | n | N | no | NO | No | n | N ) 
+            break;;
+            *) 
+            echo "Error syntax !" ;;
+    esac 
+    done
 
-mkdir $workingdirectory/css
+    mkdir $workingdirectory/css
 
-# La suite (L14) crée un fichier avec le chemin et le nom de fichier choisi (par défaut .html)
-# et génère la structure de base du site (L15 à L28) en remplissant les balises title et le href du fichier css
-cat << EOF > $workingdirectory/$htmlfilename.html
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <title>$pagetitle</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <link rel="stylesheet" href="css/$cssfilename.css" />
-    </head>
-<body>
-    <h1></h1>
-    <script src='js/$jsfilename.js' defer></script>
-</body>
-</html>
+    # La suite (L14) crée un fichier avec le chemin et le nom de fichier choisi (par défaut .html)
+    # et génère la structure de base du site (L15 à L28) en remplissant les balises title et le href du fichier css
+    cat << EOF > $workingdirectory/$htmlfilename.html
+    <!DOCTYPE html>
+    <html lang="fr">
+        <head>
+            <title>$pagetitle</title>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <link rel="stylesheet" href="css/$cssfilename.css" />
+        </head>
+    <body>
+        <h1></h1>
+        <script src='js/$jsfilename.js' defer></script>
+    </body>
+    </html>
 EOF
-# On génère ici la feuille de style .css, en récuperant la variable $cssfilename pour le nom du fichier
-cat << EOF > $workingdirectory/css/$cssfilename.css
-body{
-    margin:0;
-    padding:0;
-    box-sizing:border-box;
-}
+    # On génère ici la feuille de style .css, en récuperant la variable $cssfilename pour le nom du fichier
+    cat << EOF > $workingdirectory/css/$cssfilename.css
+    body{
+        margin:0;
+        padding:0;
+        box-sizing:border-box;
+    }
 EOF
 
-echo "[x] Fichier HTML crée avec succès."
-echo "[x] Dossier CSS crée avec succès."
-echo "[x] Fichier CSS crée avec succès."
-echo "[x] Dossier images crée avec succès."
+    echo "[x] Fichier HTML crée avec succès."
+    echo "[x] Dossier CSS crée avec succès."
+    echo "[x] Fichier CSS crée avec succès."
+    echo "[x] Dossier images crée avec succès."
+
+else 
+
+    while true
+    do
+    read -r -p "Voulez vous créer un sous-dossier JavaScript ? : " js
+
+    case "$js" in
+            oui | Oui | OUI | o | O | yes | Yes | YES | y | Y )
+            mkdir js
+            echo "[x] Dossier JS créé"
+            read -r -p "Entrez un nom pour votre fichier .js (sans écrire le .js) : " jsfilename
+            break ;;
+            non | Non | NON | n | N | no | NO | No | n | N )
+            break ;;
+            *) 
+            echo "Error syntax !" ;;
+    esac 
+    done
+
+    while true
+    do
+
+    read -r -p "Voulez vous créer un sous-dossier php ? : " php
+
+    case "$php" in
+            oui | Oui | OUI | o | O | yes | Yes | YES | y | Y )
+            mkdir php
+            echo "[x] Dossier php créé"
+            break;;
+            non | Non | NON | n | N | no | NO | No | n | N )
+            break ;;
+            *) 
+            echo "Error syntax !" ;;
+    esac 
+    done
+
+    while true 
+    do
+
+    read -r -p "Voulez vous créer un sous-dossier FONTS ? [Y/N] : " fonts 
+
+    case "$fonts" in
+            oui | Oui | OUI | o | O | yes | Yes | YES | y | Y )
+            mkdir fonts
+            echo "[x] Dossier fonts créé"
+            break;;
+            non | Non | NON | n | N | no | NO | No | n | N ) 
+            break;;
+            *) 
+            echo "Error syntax !" ;;
+    esac 
+    done
+
+    mkdir css
+
+    # La suite (L14) crée un fichier avec le chemin et le nom de fichier choisi (par défaut .html)
+    # et génère la structure de base du site (L15 à L28) en remplissant les balises title et le href du fichier css
+    cat << EOF > $htmlfilename.html
+    <!DOCTYPE html>
+    <html lang="fr">
+        <head>
+            <title>$pagetitle</title>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <link rel="stylesheet" href="css/$cssfilename.css" />
+        </head>
+    <body>
+        <h1></h1>
+        <script src='js/$jsfilename.js' defer></script>
+    </body>
+    </html>
+EOF
+    # On génère ici la feuille de style .css, en récuperant la variable $cssfilename pour le nom du fichier
+    cat << EOF > css/$cssfilename.css
+    body{
+        margin:0;
+        padding:0;
+        box-sizing:border-box;
+    }
+EOF
+
+    echo "[x] Fichier HTML crée avec succès."
+    echo "[x] Dossier CSS crée avec succès."
+    echo "[x] Fichier CSS crée avec succès."
+    echo "[x] Dossier images crée avec succès."
+fi
